2cc696cb626fbf487e8411c4ff59730db9623993
janpuc <janpuc@proton.me>
2024-06-06 17:26:36 +0200

    set vscodium to auto lint

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,11 +7,22 @@
     enableExtensionUpdateCheck = false;
 
     extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
+      # nix
+      bbenoist.nix # nix LSP
+      kamadorueda.alejandra # nix linter
     ];
 
     userSettings = {
+      # global
       "editor.fontFamily" = "'JetBrainsMono Nerd Font', monospace";
+      # nix
+      "alejandra.program" = "alejandra";
+      "[nix]" = {
+        "editor.defaultFormatter" = "kamadorueda.alejandra";
+        "editor.formatOnPaste" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnType" = false;
+      };
     };
   };
 }
