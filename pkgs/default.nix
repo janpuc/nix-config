d5f11fb45d7c657345b1c44e2b9c6a3b2dbb24f5
janpuc <janpuc@proton.me>
2025-10-13 12:08:58 -0700

    feat(pkgs): cleanup old custom pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,4 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
-  # bambu-studio = pkgs.callPackage ./bambu-studio {};
-  # beeper = pkgs.callPackage ./beeper {};
-  # microsoft-teams = pkgs.callPackage ./microsoft-teams {};
-  # orbstack = pkgs.callPackage ./orbstack {};
 }
