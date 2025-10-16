79e34c0a11a25a06926429a9c6ba344a1bfb9f5b
janpuc <janpuc@proton.me>
2025-10-16 16:20:02 +0200

    feat(proteus): enable some mac apps

--- hosts/proteus/system/default.nix
+++ hosts/proteus/system/default.nix
@@ -19,13 +19,13 @@
   };
 
   homebrew = {
-    # masApps = {
-    #   # Apps
-    #   "Steam Link" = 1246969117;
-    #   "Tailscale" = 1470499037;
-    #   # Safari addons
-    #   "Kagi for Safari" = 1558453954;
-    #   "wBlock" = 6746388723;
-    # };
+    masApps = {
+      # Apps
+      "Steam Link" = 1246969117;
+      "Tailscale" = 1475387142;
+      # Safari addons
+      "Kagi for Safari" = 1622835804;
+      # "wBlock" = 6746388723;
+    };
   };
 }
