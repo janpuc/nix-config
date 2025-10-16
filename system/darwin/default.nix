b0fd2061fdc3eec7d420226a9173364e9a166306
janpuc <janpuc@proton.me>
2025-10-16 12:01:13 +0200

    Remove chsh activation

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -56,8 +56,6 @@
   security.pam.services.sudo_local.touchIdAuth = true;
 
   system = {
-    # activationScripts run every time you boot the system or execute `darwin-rebuild`
-    activationScripts.postActivation.text = ''sudo chsh -s /run/current-system/sw/bin/fish'';
     defaults = {
       ".GlobalPreferences" = {
         ## Sets the mouse tracking speed. Found in the “Mouse” section of “System Preferences”. Set to -1.0 to disable mouse
