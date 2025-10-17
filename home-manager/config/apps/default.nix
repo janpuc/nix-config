63e8c244edb763425f6a049e5a7ca2770e4a393a
janpuc <janpuc@proton.me>
2025-10-17 11:32:44 +0200

    feat(system): add MacOS aliases for home-manager nix Apps, use current recommendation

--- home-manager/config/apps/default.nix
+++ home-manager/config/apps/default.nix
@@ -3,39 +3,106 @@
   lib,
   pkgs,
   ...
-}: let
-  apps = pkgs.buildEnv {
-    name = "home-manager-applications";
-    paths = config.home.packages;
-    pathsToLink = "/Applications";
-  };
-in {
+}: {
   # Home-manager does not link installed applications to the user environment. This means apps will not show up
   # in spotlight, and when launched through the dock they come with a terminal window. This is a workaround.
   # Upstream issue: https://github.com/nix-community/home-manager/issues/1341
-  home.activation.addApplications = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "setting up ~/Applications/Home Manager Apps..." >&2
-    nix_apps="$HOME/Applications/Home Manager Apps"
+  home.activation = {
+    checkAppManagementPermission = let
+      sudo = lib.getExe' pkgs.darwin.sudo "sudo";
+      touch = lib.getExe' pkgs.coreutils "touch";
+    in
+      lib.hm.dag.entryBefore ["linkApps"] ''
+        ensureAppManagement() {
+          for appBundle in ~/Applications/Home\ Manager\ Apps/*.app; do
+            if [[ -d "$appBundle" ]]; then
+              if ! run ${sudo} ${touch} "$appBundle/.DS_Store" &> /dev/null; then
+                return 1
+              fi
+            fi
+          done
 
-    # Delete the directory to remove old links
-    $DRY_RUN_CMD rm -rf "$nix_apps"
-    $DRY_RUN_CMD mkdir -p "$nix_apps"
+          return 0
+        }
 
-    $DRY_RUN_CMD find ${apps}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read -r src; do
-            # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
-            # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
-            # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
-            /usr/bin/osascript -e "
-                set fileToAlias to POSIX file \"$src\"
-                set applicationsFolder to POSIX file \"$nix_apps\"
+        if ! ensureAppManagement; then
+          if [[ "$(/bin/launchctl managername)" != Aqua ]]; then
+            # It is possible to grant the App Management permission to `sshd-keygen-wrapper`, however
+            # there are many pitfalls like requiring the primary user to grant the permission and to
+            # be logged in when `darwin-rebuild` is run over SSH and it will still fail sometimes...
+            printf >&2 '\e[1;31merror: permission denied when trying to update apps over SSH, aborting activation\e[0m\n'
+            printf >&2 'Apps could not be updated as `darwin-rebuild` requires Full Disk Access to work over SSH.\n'
+            printf >&2 'You can either:\n'
+            printf >&2 '\n'
+            printf >&2 '  grant Full Disk Access to all programs run over SSH\n'
+            printf >&2 '\n'
+            printf >&2 'or\n'
+            printf >&2 '\n'
+            printf >&2 '  run `darwin-rebuild` in a graphical session.\n'
+            printf >&2 '\n'
+            printf >&2 'The option "Allow full disk access for remote users" can be found by\n'
+            printf >&2 'navigating to System Settings > General > Sharing > Remote Login\n'
+            printf >&2 'and then pressing on the i icon next to the switch.\n'
+            exit 1
+          else
+            # The TCC service required to modify notarised app bundles is `kTCCServiceSystemPolicyAppBundles`
+            # and we can reset it to ensure the user gets another prompt
+            run /usr/bin/tccutil reset SystemPolicyAppBundles > /dev/null
 
-                tell application \"Finder\"
-                    make alias file to fileToAlias at applicationsFolder
-                    # This renames the alias; 'mpv.app alias' -> 'mpv.app'
-                    set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
-                end tell
-            " 1>/dev/null
-        done
-  '';
+            if ! ensureAppManagement; then
+              printf >&2 '\e[1;31merror: permission denied when trying to update apps, aborting activation\e[0m\n'
+              printf >&2 '`darwin-rebuild` requires permission to update your apps, please accept the notification\n'
+              printf >&2 'and grant the permission for your terminal emulator in System Settings.\n'
+              printf >&2 '\n'
+              printf >&2 'If you did not get a notification, you can navigate to System Settings > Privacy & Security > App Management.\n'
+              exit 1
+            fi
+          fi
+        fi
+      '';
+    linkApps = lib.hm.dag.entryAfter ["installPackages"] (
+      let
+        applications = pkgs.buildEnv {
+          name = "user-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in ''
+        targetFolder='Applications/Home Manager Apps'
+
+        echo "setting up ~/$targetFolder..." >&2
+
+        ourLink () {
+          local link
+          link=$(readlink "$1")
+          [ -L "$1" ] && [ "''${link#*-}" = 'user-applications/Applications' ]
+        }
+
+        if [ -e "$targetFolder" ] && ourLink "$targetFolder"; then
+          run rm "$targetFolder"
+        fi
+
+        run mkdir -p "$targetFolder"
+
+        rsyncFlags=(
+          # mtime is standardized in the nix store, which would leave only file size to distinguish files.
+          # Thus we need checksums, despite the speed penalty.
+          --checksum
+          # Converts all symlinks pointing outside of the copied tree (thus unsafe) into real files and directories.
+          # This neatly converts all the symlinks pointing to application bundles in the nix store into
+          # real directories, without breaking any relative symlinks inside of application bundles.
+          # This is good enough, because the make-symlinks-relative.sh setup hook converts all $out internal
+          # symlinks to relative ones.
+          --copy-unsafe-links
+          --archive
+          --delete
+          --chmod=-w
+          --no-group
+          --no-owner
+        )
+
+        run ${lib.getExe pkgs.rsync} "''${rsyncFlags[@]}" ${applications}/Applications/ "$targetFolder"
+      ''
+    );
+  };
 }
