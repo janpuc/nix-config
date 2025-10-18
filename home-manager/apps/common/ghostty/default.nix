aef62d989351050e3f0602801cb4debc287a70ef
janpuc <janpuc@proton.me>
2025-10-18 12:25:22 +0200

    feat(ghostty): set theme as Catppuccin Mocha

--- home-manager/apps/common/ghostty/default.nix
+++ home-manager/apps/common/ghostty/default.nix
@@ -1,8 +1,17 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
       unstable.ghostty-bin
     ];
+    file."${config.home.homeDirectory}/Library/Application Support/com.mitchellh.ghostty/config" = {
+      text = ''
+        command = /run/current-system/sw/bin/fish
+        click-repeat-interval = 500
+        auto-update-channel = stable
+        theme = Catppuccin Mocha
+      '';
+      force = true;
+    };
   };
 }
